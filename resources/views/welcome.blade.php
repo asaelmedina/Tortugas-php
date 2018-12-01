daa570775ffb68a4f15c8abcdab7f0ba5d38f3a8
Felipe de Jesús Rodríguez Ordóñez <868146+Neofrek@users.noreply.github.com>
2018-12-01 16:21:34 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tortugas
                 </div>
 
                 <div class="links">
